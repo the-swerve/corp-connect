25dec8a34455c03f2075b6392d89c4edd10753d5
Jay R Bolton <jayrbolton@gmail.com>
2013-11-16 02:12:35 -0800

    Rails initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Corpconnect::Application.config.secret_token = '746f6333f298ffcbbbae4d52adb181803e91c6a9b89723fc727ba63ae88dd52d2d9e3b9b9bddfe9ab463724a176603fdc92f7225ba71566689f00f1fe354dbf4'
